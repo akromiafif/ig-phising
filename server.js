e93dd9a63ccb57079e344707877a23bf34fe211d
Afif Akromi <techdevakromiafif@gmail.com>
2025-05-04 08:17:03 +0700

    feat: phising ig

--- server.js
+++ server.js
@@ -14,10 +14,13 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/instagram", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/instagram",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
